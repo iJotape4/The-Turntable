5ce90d22a4fdf4dfc8f33a4f02be47525dbe35b1
juan <jcamacho249@unab.edu.co>
2025-12-22 21:39:53 -0500

    [Add] ItemData class
    + Mock hourglass

---
+++ Source/The_Turnatable/Public/Core/Inventory/TTItem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "TTItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THE_TURNATABLE_API UTTItem : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// The display name of the item
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item")
+	FText ItemName;
+
+	// The description of the item
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item", meta=(MultiLine=true))
+	FText ItemDescription;
+
+	// The icon representing the item in the inventory
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item")
+	UTexture2D* ItemIcon;
+};
