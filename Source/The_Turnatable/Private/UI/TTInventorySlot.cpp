b1a988975461884663b183fca9297749377ccc21
juan <jcamacho249@unab.edu.co>
2026-01-02 22:36:10 -0500

    [Add] Inventory Slot - Clicking event

---
+++ Source/The_Turnatable/Private/UI/TTInventorySlot.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/TTInventorySlot.h"
+
+void UTTInventorySlot::Onclick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Clicked"));
+}
---
+++ Source/The_Turnatable/Public/UI/TTInventorySlot.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TTInventorySlot.generated.h"
+
+class UTTItem;
+/**
+ * 
+ */
+UCLASS()
+class THE_TURNATABLE_API UTTInventorySlot : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void Onclick();
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components")
+	UTTItem* Item;
+};
