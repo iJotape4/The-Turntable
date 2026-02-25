081213f3367c047f434849f85a570e4021ba40ce
JotaPe <63083839+iJotape4@users.noreply.github.com>
2026-02-24 23:23:11 -0500

    [Drop] UIUtils plugin

--- Plugins/UIUtils/Source/UIUtils/Private/UMPBackKeyWidget.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "UMPBackKeyWidget.h"
-
-void UMPBackKeyWidget::OnBackKeyPressed() const
-{
-	OnBackKeyPressedDelegate.Broadcast();
-}
